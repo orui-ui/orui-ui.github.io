33e2f9f209bbaf22a6e4a1efdc82f9f0cf79e648
xiaoRui <3399251596@qq.com>
2022-11-13 14:46:52 +0800

    修改打包配置

--- packages/orui-react/rollup.config.js
+++ packages/orui-react/rollup.config.js
@@ -1,39 +1,37 @@
 import typescript from 'rollup-plugin-typescript2';
-import less from 'rollup-plugin-less';
+// import less from 'rollup-plugin-less';
 import clear from 'rollup-plugin-clear';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
-import { uglify } from 'rollup-plugin-uglify';
+// import { uglify } from 'rollup-plugin-uglify';
 import copy from 'rollup-plugin-copy';
 import postcss from 'rollup-plugin-postcss';
 import autoprefixer from 'autoprefixer';
+import path from "path";
+
+import pkg from "./package.json";
 
 export default {
   input: ['./src/index.ts'],
   output: [
     {
-      file: 'web-react-mobile/cjs.js',
-      format: 'cjs',
-      name: 'cjs.js',
-    },
-    {
-      file: 'web-react-mobile/umd.js',
-      format: 'umd',
-      name: 'umd.js',
-    },
-    {
-      file: 'web-react-mobile/index.js',
-      format: 'es',
-      name: 'index.js',
-    },
+      // 出口文件
+      dir: path.dirname(pkg.module),
+      format: "es", // es模块导出，支持按需加载
+      name: pkg.name,
+      exports: "named", // 指定导出模式（自动、默认、命名、无）
+      preserveModules: true, // 保留模块结构
+      preserveModulesRoot: "src", // 将保留的模块放在根级别的此路径下
+   },
   ],
   plugins: [
-    typescript(),
-    less({ output: './web-react-mobile/style/index.css' }),
+    typescript({
+      tsconfig: './tsconfig.json'
+    }),
     clear({
-      targets: ['web-react'],
+      targets: ['bin'],
     }),
     resolve(),
     commonjs(),
@@ -45,23 +43,15 @@ export default {
         'src/**/*-api.tsx',
         'src/**/*.md'
       ],
-      runtimeHelpers: true,
+      // runtimeHelpers: true,
     }),
     terser(),
-    uglify(),
+    // uglify(),
     copy({
-      targets: [{ src: '../../scripts/globalStyle/compiled-colors.less', dest: 'web-react/style' }],
+      targets: [{ src: './src/Icon/assets', dest: './bin/Icon/' }],
     }),
     postcss({
       plugins: [autoprefixer()],
-      use: {
-        sass: null,
-        stylus: null,
-        less: { 
-          javascriptEnabled: true ,
-        },
-      },
-      extract: true,
     }),
   ],
   external: ['react', 'react-dom'],
